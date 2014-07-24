857f89c248c0bdc24db5511a7ade44d799382d7f
Weinrichter Norbert <weinrichter@rechnungshof.gv.at>
2014-07-24 15:07:18 +0200

    Second version (tested with matrix (1,2,3,4))

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
-## I try to invert a matrix. This is a first version
+## I try to invert a matrix. This is the second version
+
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -16,19 +17,27 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.?
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.?
 
-cacheSolve <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
+cacheinvers <- function(x, ...) {
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
         }
-        get <- function() x
-        setinverse <- function(solve) m <<- solve
-        getinverse <- function() m
-        list(set = set, get = get,
-             setinverse = setinverse,
-             getinverse = getinverse)
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        message("Berechnung der Inversen")
+        m
 }
+# Mit diesen testdaten hat das Beispiel funktioniert
+#CR <- c(1,2,3,4)
+#matrix (CR,2,2) -> Norbert
 
+#meineMatrix <- makeCacheMatrix(Norbert)
+#meineMatrix$get()
+#meineMatrix$set(matrix(CR, 1, 4))
+#meineMatrix$get()
+#cacheinvers(meineMatrix)
+#cacheinvers(meineMatrix)
